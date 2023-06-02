237346e2269cd34054ba068863d9e24bfb78085c
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-02 12:31:25 +0530

    clean main server code using routes

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 
-
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next)=>{
-    // console.log("In another Middleware");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button> </form>');
-});
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    // console.log("In another Middleware");
-    res.send("<h1>hello to Express by HSP</h1>")
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not Found</h1>');
 });
 
 app.listen(3000);
-
